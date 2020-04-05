b6fc0b37b6491a466f3a42293e45ae1be67f0f4d
prateek <prateek@MacBook-Pro.local>
2020-04-06 01:28:32 +0530

    Using terminal-notifier instead of node-notifier. Issue where the timeout is preventing Node from exiting.

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const path = require('path');
 
 const dirTree = require('directory-tree');
 
-const notifier = require('node-notifier');
+const notify = require('./notify.js')
 
 var tag = require('osx-tag');
 
 const volumesPath = '/Volumes';
 const flagToBackupTag = '__hdd_backup_dirtree';
 
-const errorNotiicationMessage = 'Error while backing up directory tree';
+const directoryTreeBackupSavePath = '/Users/prateek/Desktop/scripts/backup-directory-tree/dirTreeBackups/';
+const errorLogSavePath = '/Users/prateek/Desktop/scripts/backup-directory-tree/errors';
 
-fs.readdir(volumesPath, async (err, files) => {
-    if (err) {
+SendNotification("Backup started", "Disk Connected: Running Directory backup script...");
+
+fs.readdir(volumesPath, async (error, files) => {
+    if (error) {
         ErrorHandling(error)
     }
     for (const file of files) {
@@ -29,11 +32,13 @@ fs.readdir(volumesPath, async (err, files) => {
 const BackupDirectoryStructure = function (p) {
     try {
         const tree = dirTree(p);
-        fs.writeFile(`./dirTreeBackups/${path.basename(p)}.json`, JSON.stringify(tree, null, 2), function (err) {
-            if (err) {
+        const baseName = path.basename(p)
+        const savePath = path.join(directoryTreeBackupSavePath, `${baseName}.json`);
+        fs.writeFile(savePath, JSON.stringify(tree, null, 2), function (error) {
+            if (error) {
                 ErrorHandling(error);
             }
-            notifier.notify(`Directory Tree backup done for '${path.basename(p)}'`);
+            SendNotification('Backup successful', `Directory Tree backup done for '${baseName}'`);
         });
     } catch (error) {
         ErrorHandling(error);
@@ -43,8 +48,8 @@ const BackupDirectoryStructure = function (p) {
 const GetOSXTags = function (p) {
     return new Promise((resolve, reject) => {
         try {
-            tag.getTags(p, function (err, tags) {
-                if (err) {
+            tag.getTags(p, function (error, tags) {
+                if (error) {
                     ErrorHandling(error);
                 }
                 resolve(tags)
@@ -55,8 +60,14 @@ const GetOSXTags = function (p) {
     })
 }
 
-const ErrorHandling = function (err) {
-    notifier.notify(errorNotiicationMessage);
-    console.error(err);
-    process.exit(1)
+const ErrorHandling = function (error) {
+    const errorNotiicationMessage = 'Error while backing up directory tree';
+    SendNotification("Backup failed", errorNotiicationMessage);
+    console.error(error);
+    throw error
+}
+
+function SendNotification (subtitle, message) {
+    const title = "Backup Directory Tree"
+    notify(title, subtitle, message);
 }
---
+++ notify.js
@@ -0,0 +1,12 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+module.exports = async function (title, subtitle, message) {
+    try {
+        const terminalNotifierCommand = `terminal-notifier -title "${title}" -subtitle "${subtitle}" -message "${message}" -sound "default"`
+        await exec(terminalNotifierCommand);
+    } catch (error) {
+        console.error(error);
+        throw error
+    }
+}

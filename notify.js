716ee6f454095531303f3735ef80fbba4df30465
prateek <prateek@MacBook-Pro.local>
2020-04-06 02:11:10 +0530

    Bug Fix: Added the full path with the command

--- notify.js
+++ notify.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 
 module.exports = async function (title, subtitle, message) {
     try {
-        const terminalNotifierCommand = `terminal-notifier -title "${title}" -subtitle "${subtitle}" -message "${message}" -sound "default"`
+        const terminalNotifierCommand = `/usr/local/bin/terminal-notifier -title "${title}" -subtitle "${subtitle}" -message "${message}" -sound "default"`
         await exec(terminalNotifierCommand);
     } catch (error) {
         console.error(error);
